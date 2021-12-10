1a261671daacae521bc0c0f83646321560cfc8f2
Iacopo Melani <iacopo.melani@madeinapp.net>
2021-12-10 18:34:12 +0100

    auth/github: mocks for testing

---
+++ mock/auth.go
@@ -0,0 +1,33 @@
+package mock
+
+import (
+	"context"
+
+	"github.com/music-gang/music-gang-api/app/entity"
+	"github.com/music-gang/music-gang-api/app/service"
+)
+
+var _ service.AuthService = (*AuthService)(nil)
+
+type AuthService struct {
+	FindAuthByIDFn func(ctx context.Context, id int64) (*entity.Auth, error)
+	FindAuthsFn    func(ctx context.Context, filter service.AuthFilter) (entity.Auths, int, error)
+	CreateAuthFn   func(ctx context.Context, auth *entity.Auth) error
+	DeleteAuthFn   func(ctx context.Context, id int64) error
+}
+
+func (s *AuthService) CreateAuth(ctx context.Context, auth *entity.Auth) error {
+	return s.CreateAuthFn(ctx, auth)
+}
+
+func (s *AuthService) DeleteAuth(ctx context.Context, id int64) error {
+	return s.DeleteAuthFn(ctx, id)
+}
+
+func (s *AuthService) FindAuthByID(ctx context.Context, id int64) (*entity.Auth, error) {
+	return s.FindAuthByIDFn(ctx, id)
+}
+
+func (s *AuthService) FindAuths(ctx context.Context, filter service.AuthFilter) (entity.Auths, int, error) {
+	return s.FindAuthsFn(ctx, filter)
+}
