05fa0c42b64538340879169d3d434863ede6984e
Iacopo Melani <iacopomelani4@gmail.com>
2021-12-29 13:46:01 +0100

    tests: progress

--- tests/spec/auth_spec.rb
+++ tests/spec/auth_spec.rb
@@ -7,16 +7,57 @@ require_relative '../services/lib'
 container = service_container
 
 name = Faker::Name.name
-email = Faker::Internet.email name, '.'
+email = Faker::Internet.email name: name, separators: '.'
 password = 'Password123!'
 
 user = User.new name, email, password
 
-RSpec.describe 'Flow Auth: ' do
+describe 'Flow Auth: ' do
   describe 'register a new user' do
     context 'given correct data' do
       it 'returns token pairs' do
         container.auth_service.register user
+      end
+    end
+
+    context 'given incorrect data' do
+      context 'for example an empty name' do
+        it 'returns an error' do
+          name = ''
+
+          begin
+            container.auth_service.register User.new(name, email, password)
+          rescue ServiceError
+            'error correctley raised'
+          end
+        end
+      end
+
+      context 'for example an incorrect password' do
+        it 'returns an error' do
+          name = Faker::Name.name
+          email = Faker::Internet.email name: name, separators: '.'
+          password = 'non-valid-password'
+
+          begin
+            container.auth_service.register User.new(name, email, password)
+          rescue ServiceError
+            'error correctley raised'
+          end
+        end
+      end
+
+      context 'for example an incorrect email' do
+        it 'returns an error' do
+          name = Faker::Name.name
+          email = 'incorrect-email'
+
+          begin
+            container.auth_service.register User.new(name, email, password)
+          rescue ServiceError
+            'error correctley raised'
+          end
+        end
       end
     end
   end
