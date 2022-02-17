8ee895efb1eee3e807f318dfde22406477f16e07
Iacopo Melani <iacopomelani4@gmail.com>
2022-02-17 18:55:59 +0100

    postgres:  started contract service postgres

--- app/entity/contract.go
+++ app/entity/contract.go
@@ -35,14 +35,14 @@ type Contracts []*Contract
 // The contract is a cloud function that is executed on a server, deployed by users;
 // The contract can have multiple revisions.
 type Contract struct {
-	ID          int64       `json:"id"`
-	Name        string      `json:"name"`
-	Description null.String `json:"description"`
-	UserID      null.Int    `json:"user_id"`
-	Visibility  Visibility  `json:"visibility"`
-	MaxFuel     Fuel        `json:"max_fuel"` // The maximum amount of fuel that can be burned from the contract.
-	CreatedAt   time.Time   `json:"created_at"`
-	UpdatedAt   time.Time   `json:"updated_at"`
+	ID          int64      `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	UserID      null.Int   `json:"user_id"`
+	Visibility  Visibility `json:"visibility"`
+	MaxFuel     Fuel       `json:"max_fuel"` // The maximum amount of fuel that can be burned from the contract.
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
 
 	LastRevision *Revision `json:"last_revision"`
 	User         *User     `json:"user"`
