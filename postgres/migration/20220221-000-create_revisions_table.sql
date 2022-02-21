3b72ea8ce00c92d1d9bac60b5ba62016f4f42470
Iacopo Melani <iacopomelani4@gmail.com>
2022-02-21 23:52:43 +0100

    postgres: create revisions table

---
+++ postgres/migration/20220221-000-create_revisions_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE revisions
+(
+    id BIGSERIAL PRIMARY KEY,
+    rev INT NOT NULL,
+    version VARCHAR(255) NOT NULL,
+    contract_id BIGINT NOT NULL REFERENCES contracts(id) ON DELETE CASCADE,
+    note TEXT NOT NULL DEFAULT '',
+    code TEXT NOT NULL DEFAULT '',
+    compiled_code BYTEA NOT NULL,
+    hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+
+);
+
+CREATE UNIQUE INDEX UQ_CONTRACT_REVISION on revisions(rev, contract_id);
