0934bc5684d33713a3d934c4d2b21192c5762886
Iacopo Melani <iacopomelani4@gmail.com>
2022-08-10 18:06:41 +0200

    app/util: generic safe RunningState helper

---
+++ app/util/running_state.go
@@ -0,0 +1,18 @@
+package util
+
+import "sync/atomic"
+
+// RunningState is a struct that can be used as a safe running state flag for a goroutine.
+type RunningState struct {
+	running int32
+}
+
+// IsRunning returns true if the SafeRunningState is running
+func (s *RunningState) IsRunning() bool {
+	return atomic.LoadInt32(&s.running) == 1
+}
+
+// SetRunningState sets the running state of the FuelStation.
+func (s *RunningState) SetRunningState(val int32) {
+	atomic.StoreInt32(&s.running, val)
+}
